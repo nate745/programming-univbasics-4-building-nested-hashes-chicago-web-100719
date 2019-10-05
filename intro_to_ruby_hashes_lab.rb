9e9d403789bda98ca67009c9af46195bfc0ee1f5
Nathan Alex <nate745@gmail.com>
2019-10-05 08:58:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,9 +29,7 @@ def monopoly_with_third_tier
 	    :names=>
 	    {
 	      :reading_railroad=>
-	      {
-	        "mortgage_value"=>"$100"
-	      },
+	      {},
 	      :pennsylvania_railroad=>
 	      {
 	        "mortgage_value"=>"$200"

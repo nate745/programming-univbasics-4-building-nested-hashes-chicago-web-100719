8930e743fc663bfb204dc40d362f2d38a7783f7b
Nathan Alex <nate745@gmail.com>
2019-10-05 08:52:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,14 +28,26 @@ def monopoly_with_third_tier
 			},
 			:names=>
 			{
-				:reading_railroad=>{},
-				:pennsylvania_railroad=>{},
-				:mortgage_value=>{},
-				:shortline=>{},
+				:reading_railroad=>
+				{
+					"mortgage_value"=>"$100"
+				},
+				:pennsylvania_railroad=>
+				{
+					"mortgage_value"=>"$200"
+				},
+				:b_and_o_railroad=>
+				{
+					"mortgage_value"=>"$400"
+				},
+				:shortline=>
+				{
+					"mortgage_value"=>"$800"
 				}
 			}
+		}
 	}
-}
+
 
 end
 
